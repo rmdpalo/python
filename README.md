c22d97adbf6a7b238512d7ce1e44af0fbd270b53
rmdpalo <>
2021-05-10 17:11:39 +0000

    adds hello-world and instructions on how to run files in README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python
+
+Notes as I learn the Python programming language
+
+to run specific files change the run command into the proper .py file
+
+#### ./.replit
+```
+language = "python3"
+run = "python3 hello-world/main.py"
+```
---
+++ hello-world/main.py
@@ -0,0 +1,13 @@
+#print statements and program flow
+
+#print() prints something into the console
+
+print("Hello world!")
+
+num = 2
+print(num)
+
+#above code prints:
+#Hello world!
+#2
+#In that order because the computer reads the code from top to bottom.
